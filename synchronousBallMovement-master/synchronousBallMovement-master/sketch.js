30f14f73a94a177cb1b83f97ef77587be3d71b2c
RishitShekhar <71666570+RishitShekhar@users.noreply.github.com>
2021-01-28 13:52:41 -0600

    Add files via upload

---
+++ synchronousBallMovement-master/synchronousBallMovement-master/sketch.js
@@ -0,0 +1,16 @@
+var canvas, backgroundImage,playerCount,database,form,game,player;
+var gameState = 0;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+
+
+}
+
